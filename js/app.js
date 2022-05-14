c58364b6c72382af485f9b6bfe91a7f6cba5d760
topgirl2410 <maria.topgirl82@gmail.com>
2022-05-14 19:36:31 +0200

    trabajando con la clase ui

--- js/app.js
+++ js/app.js
@@ -18,19 +18,51 @@ function eventListeners() {
 
 // Clases 
 
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
+
+class UI {
+    insertarPresupuesto(cantidad) {
+
+        // Extreayendo los valores
+        const { presupuesto, restante} = cantidad;
+
+        // Agregamos los valores al HTML
+        document.querySelector('#total').textContent = presupuesto;
+        document.querySelector('#restante').textContent = restante;
+    }
+ }
 
 
+// Instanciar 
+const ui = new UI();
+let presupuesto;
 
 // Funciones
 
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('¿Cual es tu presupuesto semanal?');
 
-    console.log(Number(presupuestoUsuario));
-
-    if (presupuestoUsuario === '' || presupuestoUsuario === null  || isNaN(presupuestoUsuario)) {
+    if (presupuestoUsuario === ''
+        || presupuestoUsuario === null
+        || isNaN(presupuestoUsuario)
+        || presupuestoUsuario <= 0) {
         window.location.reload();
     }
+
+    console.log(Number(presupuestoUsuario));
+
+    // Presupuesto valido
+
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    //console.log(presupuesto);
+
+    ui.insertarPresupuesto(presupuesto);
 }
 
 
